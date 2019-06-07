43ab9522f2b1fcc56b1a454023f7029f5afbed32
yu2749luca <yu2749luca@hotmail.com>
2019-06-07 00:13:10 -0700

    first push

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express'); //get express package
+const bodyParser = require('body-parser');
+const bcrypt = require ('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+//access of handlers
+const register = require('./controller/register');
+const signin = require('./controller/signin');
+const profile = require('./controller/profile');
+const image = require('./controller/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+const app = express(); //run express
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{res.send(database.users)})
+app.post('/SignIn',(req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+app.post('/register', (req,res)  => {register.handleRegister (req,res,db,bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req,res,db)})
+app.put('/image',(req,res)=> {image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall (req,res)})
+
+app.listen(3000,()=>{
+	console.log('app is running on port 3000 ');
+})
